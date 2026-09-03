b23e9d9e09fca724f06a38e1e8e9b8b3193bdd9e
Carlos Alloatti <calloatti@gmail.com>
2026-09-03 12:52:13 -0300

    Update 6 files
    2026-09-03 12:52

---
+++ Version-1.1/Source/ModStarter.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Timberborn.Modding;
+using Timberborn.ModManagerScene;
+
+namespace Calloatti.PausableStorage
+{
+  public class PausableStorageModStarter : IModStarter
+  {
+    public void StartMod(IModEnvironment modEnvironment)
+    {
+      // Re-enable Harmony to inject our custom input/output logic rules
+new Harmony("Calloatti.PausableStorage").PatchAll();
+    }
+  }
+}
